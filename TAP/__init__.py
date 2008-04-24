1df1a91cadef4d38929e9f09d3f60b97980ad22c
erk <erk>
2008-04-24 12:23:38 +0000

    Complete file output for TAP

--- TAP/__init__.py
+++ TAP/__init__.py
@@ -4,17 +4,18 @@ import atexit
 # todo: make written-to stream passable
 
 class Plan(object):
-  def __init__(self, plan, param=None):
+  def __init__(self, plan, param=None,out=sys.stdout):
     self.counter = 0
     self.expected_tests = None
     self.ended = False
+    self.out = out
 
     if isinstance(plan, int):
       self.expected_tests = plan
-      print "1..%u" % self.expected_tests
+      self.out.write("1..%u\n" % self.expected_tests)
     elif plan == "no_plan" or plan == None: 1
     elif plan == "skip_all":
-      print "1..0 # skip %s" % param
+      self.out.write("1..0 # skip %s\n" % param)
       raise SystemExit(0) # ??? this is what T::B does, but sucks
     else:
       raise TestBadPlan(plan)
@@ -26,10 +27,10 @@ class Plan(object):
     if self.ended: return
     self.ended = True
     if self.expected_tests is None:
-      print "1..%u" % self.counter
+      self.out.write("1..%u\n" % self.counter)
     elif self.counter != self.expected_tests:
-      print "# Looks like you planned %u tests but ran %u." \
-        % (self.expected_tests, self.counter)
+      self.out.write("# Looks like you planned %u tests but ran %u.\n" \
+        % (self.expected_tests, self.counter))
 
 class Builder(object):
   global_defaults = {
@@ -48,14 +49,15 @@ class Builder(object):
   @classmethod # XXX: why did this fail?
   def create(cls, plan=None, plan_param=None, out=sys.stdout):
     # self = new.instance(cls) # ? this sucks, too
-    self = Builder(out=out)
+    self = Builder()
     self.__dict__  = self.global_defaults.copy()
+    self.out = out
     if plan: self.set_plan(plan, plan_param)
     return self
 
   def set_plan(self, plan, plan_param=None):
     if self.get_plan(): raise TestPlannedAlready(plan, plan_param)
-    self._plan = Plan(plan, plan_param)
+    self._plan = Plan(plan, plan_param,out=self.out)    
     atexit.register(self._plan.__del__)
 
   def get_plan(self): return self._plan
@@ -65,14 +67,13 @@ class Builder(object):
     if is_ok: report = "ok" 
     else:     report = "not ok"
 
-    sys.stdout.write("%s %u" % (report, self.current))
+    self.out.write("%s %u" % (report, self.current))
 
     if desc: self.out.write(" - %s" % desc)
     if skip: self.out.write(" # SKIP %s" % skip)
     if todo: self.out.write(" # TODO %s" % todo)
 
-    print
-
+    self.out.write("\n")
     self.current += 1
 
   def reset(self):
